f30739254be110d8ab2155ff13fe70b98510f20f
KIRA <kanavraina98@gmail.com>
2018-09-20 02:05:39 +0530

    Update net_scanner.py
    error in sixth line ,
    mac address is of 48 bits not 32 bits

--- net_scanner.py
+++ net_scanner.py
@@ -3,7 +3,7 @@ import scapy.all as scapy
 def scan(ip):
     arp_request=scapy.ARP(pdst=ip)
 
-    broadcast=scapy.Ether(dst="ff:ff:ff:ff")
+    broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast=broadcast/arp_request
     answered_list=scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
     print("IP\t\t\tMAC address\n----------------------------------------")
