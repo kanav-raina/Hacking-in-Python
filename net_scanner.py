c767290111a3b4e76259bf4880c8a794bd77f52f
KIRA <kanavraina98@gmail.com>
2018-09-19 19:14:48 +0530

    Update net_scanner.py

--- net_scanner.py
+++ net_scanner.py
@@ -1,14 +1,19 @@
 import scapy.all as scapy
 
 def scan(ip):
-    arp_request=scapy.ARP(pdst=ip)           
+    arp_request=scapy.ARP(pdst=ip)
 
-    broadcast=scapy.Ether(dst="ff:ff:ff:ff") 
-    arp_request_broadcast=broadcast/arp_request   
-    answered_list=scapy.srp(arp_request_broadcast,timeout=1)[0]    
+    broadcast=scapy.Ether(dst="ff:ff:ff:ff")
+    arp_request_broadcast=broadcast/arp_request
+    answered_list=scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+    print("IP\t\t\tMAC address\n----------------------------------------")
+    clients_list=[]
     for element in answered_list:
-        print(element[1].psrc)    
-        print(element[1].hwsrc)    
-        print("---------------------------------------")
-ip =input("Enter your ip : ")
-scan(ip)
+        client_dict={"ip":element[1].psrc,"mac":element[1].hwsrc}
+        clients_list.append(client_dict)
+        return clients_list
+
+ip=input("Enter your ip")
+arr=scan(ip)
+for i in range(len(arr)):
+    print(arr[i]["ip"]+"\t\t"+arr[i]["mac"])
